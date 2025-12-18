2ae0012fdca9a6014d4512863beec977aa8832e5
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-12-18 01:01:11 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/nav/nav.dart
+++ lib/flutter_flow/nav/nav.dart
@@ -87,20 +87,6 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
               appStateNotifier.loggedIn ? NavBarPage() : LoginWidget(),
         ),
         FFRoute(
-          name: LoginWidget.routeName,
-          path: LoginWidget.routePath,
-          builder: (context, params) => LoginWidget(),
-        ),
-        FFRoute(
-            name: TasksWidget.routeName,
-            path: TasksWidget.routePath,
-            builder: (context, params) => params.isEmpty
-                ? NavBarPage(initialPage: 'tasks')
-                : NavBarPage(
-                    initialPage: 'tasks',
-                    page: TasksWidget(),
-                  )),
-        FFRoute(
           name: OnboardingWidget.routeName,
           path: OnboardingWidget.routePath,
           builder: (context, params) => OnboardingWidget(),
@@ -126,6 +112,20 @@ GoRouter createRouter(AppStateNotifier appStateNotifier) => GoRouter(
                 : NavBarPage(
                     initialPage: 'completed',
                     page: CompletedWidget(),
+                  )),
+        FFRoute(
+          name: LoginWidget.routeName,
+          path: LoginWidget.routePath,
+          builder: (context, params) => LoginWidget(),
+        ),
+        FFRoute(
+            name: TasksWidget.routeName,
+            path: TasksWidget.routePath,
+            builder: (context, params) => params.isEmpty
+                ? NavBarPage(initialPage: 'tasks')
+                : NavBarPage(
+                    initialPage: 'tasks',
+                    page: TasksWidget(),
                   ))
       ].map((r) => r.toRoute(appStateNotifier)).toList(),
     );
